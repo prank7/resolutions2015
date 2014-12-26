d4c36e14f3e6764975cfb28a27fd79915349c9e8
prank7 <prashant.abhishek7g@gmail.com>
2014-12-26 19:13:40 +0530

    Herkou assets

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, '425078167639341', '0ff50e9f1e46fa6ee135d35d5ca62905'
+  provider :facebook, ENV["FB_CLIENT"], ENV["FB_SECRET"]
 end
